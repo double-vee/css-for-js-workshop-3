b202911573547b51cce9fea4eee27593f20b33e8
Weronika Kansy <weronika.kansy@gmail.com>
2023-09-28 03:17:53 +0200

    Update Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -13,39 +13,54 @@ const Select = ({ label, value, onChange, children }) => {
       <SelectWrapper value={value} onChange={onChange}>
         {children}
       </SelectWrapper>
-      <IconWrapper>
-        <Icon id="chevron-down" strokeWidth={2} size={12} />
-      </IconWrapper>
+      <VisibleWrapper>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" strokeWidth={2} size={16} />
+        </IconWrapper>
+      </VisibleWrapper>
     </Container>
   );
 };
 
 const Container = styled.div`
+  width: max-content;
   position: relative;
-  width: fit-content;
+`;
+
+const SelectWrapper = styled.select`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  appearance: none;
+  opacity: 0;
+`;
+
+const VisibleWrapper = styled.div`
+  border-radius: 8px;
+  padding: 12px 48px 12px 16px;
+  background-color: ${COLORS.transparentGray15};
   color: ${COLORS.gray700};
 
-  &:hover {
+  ${SelectWrapper}:focus + & {
+    outline: 2px solid #000;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${SelectWrapper}:hover + & {
     color: ${COLORS.black};
   }
 `;
 
-const SelectWrapper = styled.select`
-  appearance: none;
-  padding: 12px 46px 12px 16px;
-  border: none;
-  border-radius: 8px;
-  background-color: ${COLORS.transparentGray15};
-  color: inherit;
-`;
-
 const IconWrapper = styled.div`
-  width: 12px;
-  height: 12px;
+  width: 16px;
+  height: 16px;
   position: absolute;
-  right: 13px;
   top: 0;
   bottom: 0;
+  right: 12px;
   margin: auto;
   pointer-events: none;
 `;
